01a009557a8a5b0d93534415f4bc0369b14945f4
김동열 <ehfql6363@naver.com>
2025-08-10 16:56:52 +0900

    feat: 라운드 표시

--- src/main/java/kwangdong/pingplugin/util/WaveTags.java
+++ src/main/java/kwangdong/pingplugin/util/WaveTags.java
@@ -4,10 +4,12 @@ import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.Plugin;
 
 public final class WaveTags {
-	private WaveTags() {}
-	public static NamespacedKey WAVE_MOB;
+    private WaveTags() {}
+    public static NamespacedKey WAVE_MOB;
+    public static NamespacedKey WAVE_ROUND; // 라운드 표시
 
-	public static void init(Plugin plugin) {
-		WAVE_MOB = new NamespacedKey(plugin, "wave_mob");
-	}
+    public static void init(Plugin plugin) {
+        WAVE_MOB  = new NamespacedKey(plugin, "wave_mob");
+        WAVE_ROUND = new NamespacedKey(plugin, "wave_round");
+    }
 }
